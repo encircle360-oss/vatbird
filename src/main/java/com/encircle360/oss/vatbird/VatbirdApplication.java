c381263de297ad9c959f052092196d7e718231d1
Patrick Hütter <p.huetter@encircle360.com>
2020-03-26 23:32:04 +0100

    Initial commit

---
+++ src/main/java/com/encircle360/oss/vatbird/VatbirdApplication.java
@@ -0,0 +1,13 @@
+package com.encircle360.oss.vatbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VatbirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VatbirdApplication.class, args);
+	}
+
+}
